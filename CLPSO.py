1f2258c764cbba5a3d1fca5fd7fdcf6aa095a23c
Thu Huynh Van <65479151+thuchula6792@users.noreply.github.com>
2021-08-03 11:16:48 +0700

    Update CLPSO.py

--- CLPSO.py
+++ CLPSO.py
@@ -21,16 +21,16 @@ def func1(x):
 #--- MAIN 
 class Particle:
     def __init__(self):
-        self.position_i=[]          # particle position
-        self.velocity_i=[]          # particle velocity
-        self.pos_best_i=[]          # best position individual
-        self.pos_best_i_record=[]   # particles x dimensions NP*D
-        self.err_best_i= 1000000    # best error individual
-        self.err_best_i_record=[]
-        self.err_i=-1               # error individual
-        for i in range(0,num_dimensions):
-            self.velocity_i.append(random.uniform(-1,1)*(bounds[i][1]-bounds[i][0])+bounds[i][0])
-            self.position_i.append(random.uniform(-1,1)*(bounds[i][1]-bounds[i][0])+bounds[i][0])
+        self.position_i = []          # particle position
+        self.velocity_i = []          # particle velocity
+        self.pos_best_i = []          # best position individual
+        self.pos_best_i_record = []   # particles x dimensions NP*D
+        self.err_best_i = 1000000    # best error individual
+        self.err_best_i_record = []
+        self.err_i = -1               # error individual
+        for i in range(0, num_dimensions):
+            self.velocity_i.append(random.uniform(-1,1)*(bounds[i][1] - bounds[i][0]) + bounds[i][0])
+            self.position_i.append(random.uniform(-1,1)*(bounds[i][1] - bounds[i][0]) + bounds[i][0])
         self.pos_best_i_record.append(self.position_i)
         
     # Evaluate current fitness
@@ -46,12 +46,12 @@ class Particle:
         c1 = 2        # cognative constant
         c2 = 2        # social constant
         for i in range(0, num_dimensions):
-            r1=random.random()
-            r2=random.random()
+            r1 = random.random()
+            r2 = random.random()
             vel_cognitive = c1*r1*(pbest_f[i] - self.position_i[i])
-            vel_social=c2*r2*(pos_best_g[i]-self.position_i[i])
+            vel_social = c2*r2*(pos_best_g[i] - self.position_i[i])
             self.velocity_i[i] = mdblI*self.velocity_i[i] + vel_cognitive + vel_social
-            self.velocity_i[i] = np.where(self.velocity_i[i] >= 0.2*(bounds[i][1]-bounds[i][0]), 0.2*(bounds[i][1]-bounds[i][0]), self.velocity_i[i]).tolist()
+            self.velocity_i[i] = np.where(self.velocity_i[i] >= 0.2*(bounds[i][1] - bounds[i][0]), 0.2*(bounds[i][1] - bounds[i][0]), self.velocity_i[i]).tolist()
     
     # Update the particle position based off new velocity updates
     def update_position(self, bounds):
@@ -63,7 +63,7 @@ class Particle:
             # Adjust minimum position if neseccary
             if self.position_i[i] < bounds[i][0]:
                 self.position_i[i] = bounds[i][0]      
-        
+           
 class CLPSO():
     def __init__(self, costFunc, x0, bounds, num_particles, maxiter):
         
@@ -80,14 +80,14 @@ class CLPSO():
         self.mintNuC = 5; self.mdblI = 0
         
         # Learning probability Pc
-        t=np.linspace(0,5,self.num_particles)
-        self.swarm=[]
-        for i in range(0,num_particles):
+        t = np.linspace(0, 5, self.num_particles)
+        self.swarm = []
+        for i in range(0, num_particles):
             self.swarm.append(Particle())   
-            self.Pc.append(0+0.5*(np.exp(t[i])-np.exp(t[0]))/(np.exp(5)-np.exp(t[0])))
+            self.Pc.append(0 + 0.5*(np.exp(t[i]) - np.exp(t[0])) / (np.exp(5) - np.exp(t[0])))
             self.f_pbest.append([i]*num_dimensions)
             
-        for k in range(0,self.num_particles):
+        for k in range(0, self.num_particles):
             self.swarm[k].evaluate(costFunc)
             
             if self.swarm[k].err_i < self.err_best_g:  
@@ -97,10 +97,9 @@ class CLPSO():
             self.err_best_g_record.append(self.swarm[k].err_best_i)
             self.pbest_f.append(self.swarm[k].position_i)
             
-        self.f_pbest = self.Comprehensive_learning(self.num_particles, self.Pc, self.err_best_g_record, self.f_pbest)   
-            
     def Comprehensive_learning(self, num_particles, Pc, err_best_g_record, f_pbest):
-        # Generate exemplars for each dimension
+        
+        # Generate exemplar for each dimension
         for v in range(0, num_particles):
             if self.mintSinceLastChange[v] > self.mintNuC:
                self.mintSinceLastChange[v] = 0
@@ -111,7 +110,7 @@ class CLPSO():
                  self.bi1 = random.random() - 1 + Pc[v]
                  self.bi[z] = np.where(self.bi1 >= 0, 1, 0).tolist()   
             if np.sum(self.bi) == 0:
-                rc = round(random.uniform(0,1)*(num_dimensions-1))
+                rc = round(random.uniform(0,1)*(num_dimensions - 1))
                 self.bi[rc] = 1
             for m in range(0,num_dimensions):
                 f_pbest[v][m] = self.bi[m]*self.fi[m] + (1 - self.bi[m])*f_pbest[v][m]
@@ -123,14 +122,14 @@ class CLPSO():
         while i < self.maxiter:
             self.iteration.append(i)
             self.pos_best_g_record_1 = np.copy(self.pos_best_g_record)
-            self.pbest_f_1=np.copy(self.pbest_f)
+            self.pbest_f_1 = np.copy(self.pbest_f)
             
             # Perform comprehensive learning strategy
             self.f_pbest = self.Comprehensive_learning(self.num_particles, self.Pc, self.err_best_g_record, self.f_pbest)
             
             # Learning from exemplars
             for j in range(0, self.num_particles):
-                 for k in range(0,num_dimensions):
+                 for k in range(0, num_dimensions):
                          index_1 = self.f_pbest[j][k]
                          self.pbest_f_1[j,k] = self.pos_best_g_record_1[index_1, k]
             self.pbest_f = self.pbest_f_1.tolist()
@@ -156,10 +155,11 @@ class CLPSO():
                     self.err_best_g = float(self.swarm[j].err_i)
             self.global_best_all_iteration.append(self.err_best_g)
             i+=1
+
         # Final results
         print('FINAL RESULTS:', self.pos_best_g, '---', self.err_best_g)
-          
-initial=[-1, 1]   # Initial starting location [x1,x2...]
-bounds=[(-2.08, 2.08),(-2.08, 2.08)] 
-t = CLPSO(func1, initial, bounds, num_particles=20, maxiter=500)
+        
+initial = [-1, 1]   # Initial starting location [x1,x2...]
+bounds = [(-2.08, 2.08),(-2.08, 2.08)] 
+t = CLPSO(func1, initial, bounds, num_particles = 10, maxiter = 700)
 t.Run(func1)
